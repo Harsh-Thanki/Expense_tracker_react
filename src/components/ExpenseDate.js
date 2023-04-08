5e127308f598622b5912763824ec9bdd18eae589
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-08 10:59:04 +0530

    ExpenseDate css applied.

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -7,8 +7,10 @@ function ExpenseDate(props){
     const year = props.date.getFullYear();
 
     return (
-        <div className='DateText'>
-            {month} {day}, {year}
+        <div className='expense-date'>
+            <div className='expense-date__month'>{month}</div>
+            <div className='expense-date__day'>{day}</div>
+            <div className='expense-date__year'>{year}</div>
         </div>
     );
 }
