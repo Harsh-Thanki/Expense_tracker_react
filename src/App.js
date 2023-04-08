64488125961350e9acba081eb02ec4aed0603964
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-08 08:05:57 +0530

    data passed to ExpenseItem component(props).

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  let expenseDate = new Date(2022, 12, 27);
+  let expenseTitle = "Car Insurence";
+  let expenseAmount = 350;
+
   return (
     <div>
       <h1>List of Expenses</h1>
-      <ExpenseItem/>
+      <ExpenseItem date={expenseDate} title={expenseTitle} amount={expenseAmount}/>
     </div>
   );
 }
 
 export default App;
+
