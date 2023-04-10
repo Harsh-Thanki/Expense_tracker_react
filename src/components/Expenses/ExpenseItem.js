0d4a54360311493d245739f439ff5dc999bfb270
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-10 14:05:26 +0530

    A Button and it's event hander added.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,10 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+    const btn_click_handler = () => {
+        alert("Button Clicked!!");
+    }
+
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
@@ -11,6 +15,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={btn_click_handler}>Edit</button>
         </Card>
     );
 }
