3774c718e5570a55e73d97cd3524f3f5979786b7
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-10 14:12:27 +0530

    Title made auto-updating on button click.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import {React, useState} from 'react';
 
 const ExpenseItem = (props) => {
 
+    const [item_title, set_item_title] = useState(props.title);
+
     const btn_click_handler = () => {
-        alert("Button Clicked!!");
+        set_item_title("New Title");
     }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{item_title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={btn_click_handler}>Edit</button>
@@ -21,3 +24,4 @@ const ExpenseItem = (props) => {
 }
 
 export default ExpenseItem;
+
