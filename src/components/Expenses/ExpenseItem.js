b61d5a0f4aaebf741885c6b749c20fca1f0ddf05
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-10 15:11:01 +0530

    user input reflected to title.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,11 +4,14 @@ import Card from '../UI/Card';
 import {React, useState} from 'react';
 
 const ExpenseItem = (props) => {
-
+    const [new_item_title, set_new_item_title] = useState("");
     const [item_title, set_item_title] = useState(props.title);
 
+    const txt_change_handler = (event) => {
+        set_new_item_title(event.target.value);
+    }
     const btn_click_handler = () => {
-        set_item_title("New Title");
+        set_item_title(new_item_title);
     }
 
     return (
@@ -18,6 +21,7 @@ const ExpenseItem = (props) => {
                 <h2>{item_title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <input type='Text' onChange={txt_change_handler} value={new_item_title}/>
             <button onClick={btn_click_handler}>Edit</button>
         </Card>
     );
