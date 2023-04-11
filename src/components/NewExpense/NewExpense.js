480f316a20a4532603f6aae8900934730122718f
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-11 14:45:14 +0530

    get_expense_form_data function defined and sent to ExpenseForm component.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,9 +2,14 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
+
+    const get_expense_form_data = (form_data) => {
+        console.log(form_data);
+    };
+
     return (
         <div className='new-expense'>
-            <ExpenseForm/>
+            <ExpenseForm expense_form_data = {get_expense_form_data}/>
         </div>
     );
 }
