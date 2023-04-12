ed1601f01af84593564f9668638db4a2ee17ee50
Harsh-Thanki <thankiharsh7529@gmail.com>
2023-04-12 13:02:27 +0530

    whole array of object sent to ExpenseItem component for display instead of first five.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,11 +5,16 @@ import Card from '../UI/Card';
 const Expenses = (props) => {
     return(
         <Card className='expenses'>
-            <ExpenseItem date={props.items[0].date} title={props.items[0].title} amount={props.items[0].amount}/>
-            <ExpenseItem date={props.items[1].date} title={props.items[1].title} amount={props.items[1].amount}/>
-            <ExpenseItem date={props.items[2].date} title={props.items[2].title} amount={props.items[2].amount}/>
-            <ExpenseItem date={props.items[3].date} title={props.items[3].title} amount={props.items[3].amount}/>
-            <ExpenseItem date={props.items[4].date} title={props.items[4].title} amount={props.items[4].amount}/>
+            {
+                props.items.map(
+                    expense => (
+                        <ExpenseItem 
+                        date={expense.date}
+                        title={expense.title} 
+                        amount={expense.amount}/>
+                    )
+                )
+            }
         </Card>
     );
 }
